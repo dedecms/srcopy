9d7901ab0ce479a26dfb57fef30a7572be9e9b03
Code <mycalf@me.com>
2021-03-31 13:42:48 +0800

    update

--- main.go
+++ main.go
@@ -4,10 +4,12 @@ import (
 	"fmt"
 	"log"
 	"os"
+	"strconv"
 	"strings"
 
 	"github.com/dedecms/srcopy/snake"
 	"github.com/jung-kurt/gofpdf"
+	"github.com/pdfcpu/pdfcpu/pkg/api"
 	"github.com/urfave/cli/v2"
 )
 
@@ -32,6 +34,8 @@ func main() {
 			&cli.StringFlag{Name: "name", Aliases: []string{"n"}, Usage: "软件名。"},
 			&cli.StringFlag{Name: "shor", Aliases: []string{"s"}, Usage: "简称或英文名。"},
 			&cli.StringFlag{Name: "ver", Usage: "软件版本号。"},
+			&cli.StringFlag{Name: "sp", Value: "30", Usage: "保存前多少页"},
+			&cli.StringFlag{Name: "lp", Value: "30", Usage: "保存后多少页"},
 			&cli.StringFlag{Name: "path", Aliases: []string{"p"}, Usage: "源文件位置。"},
 			&cli.StringFlag{Name: "files", Value: "*.php,*.htm", Aliases: []string{"f"}, Usage: "文件列表，用','进行分割，可使用通配符，如：'*.go'。"},
 			&cli.BoolFlag{Name: "dir", Value: false, Aliases: []string{"d"}, Usage: "自动对当前目录下的子目录进行批处理。"},
@@ -48,11 +52,11 @@ func main() {
 			if c.Bool("dir") {
 				for _, l := range snake.FS(path).Ls() {
 					if i := snake.FS(l); i.IsDir() && strings.Index(i.Get(), ".") != 0 {
-						savePDF(mergeCodes(l, exts...), n, l)
+						savePDF(mergeCodes(l, exts...), n, l, c.Int("sp"), c.Int("lp"))
 					}
 				}
 			} else {
-				savePDF(mergeCodes(path, exts...), n, path)
+				savePDF(mergeCodes(path, exts...), n, path, c.Int("sp"), c.Int("lp"))
 			}
 
 			return nil
@@ -67,15 +71,15 @@ func main() {
 }
 
 // 保存Docx文件
-func savePDF(src, name, out string) {
+func savePDF(src, name, out string, sp, lp int) {
 	f := snake.Text(src).ReComment()
 
 	pdf := gofpdf.New("P", "mm", "A4", "")
 	pdf.AddUTF8Font("NotoSansSC", "", "./font/NotoSansSC-Regular.ttf")
 	name = snake.Text(name).Trim(" ").Get() + " " + snake.FS(out).Base()
 	name = snake.Text(name).Trim(" ").Get()
-
-	fmt.Println(name, "总行数：", len(f.Lines()))
+	lsc := len(f.Lines())
+	fmt.Println(name, "总行数：", lsc)
 
 	pdf.SetTitle(name, false)
 	pdf.SetAuthor("Jules Verne", false)
@@ -116,7 +120,22 @@ func savePDF(src, name, out string) {
 		}
 
 	}
-	pdf.OutputFileAndClose(out + "/../" + name + ".pdf")
+
+	snake.FS(out + "/../srcopy/src/").MkDir()
+	inFile := out + "/../srcopy/src/" + name + " [总行数：" + strconv.Itoa(lsc) + "].pdf"
+	outFile := out + "/../srcopy/" + name + " [总行数：" + strconv.Itoa(lsc) + "].pdf"
+
+	pdf.OutputFileAndClose(inFile)
+
+	api.OptimizeFile(inFile, "", nil)
+
+	if pdf.PageCount() > (lp + sp) {
+		lp = pdf.PageCount() - lp
+		sp = sp + 1
+		api.RemovePagesFile(inFile, outFile, []string{strconv.Itoa(sp) + "-" + strconv.Itoa(lp)}, nil)
+		api.OptimizeFile(outFile, "", nil)
+	}
+
 }
 
 // 根据目录，将所选择的文件合并.
